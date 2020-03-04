6df59f177786b0c6eeeb874fc7fdebc18a75ad31
acepMunawar <acepmunawar96@gmail.com>
2020-03-03 19:33:37 -0500

    Initial commit

---
+++ app/src/main/java/com/Education/dasarkode/ScreenFlash.java
@@ -0,0 +1,31 @@
+package com.Education.dasarkode;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class ScreenFlash extends AppCompatActivity {
+    private int duration = 8000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_flash);
+        screenSplash();
+    }
+
+    public void screenSplash(){
+        new Handler().postDelayed(new Runnable() {
+                                      @Override
+                                      public void run() {
+                                          Intent home =new Intent(ScreenFlash.this, home.class);
+                                          startActivity(home);
+                                          finish();
+                                      }
+                                  },duration
+        );
+    }
+
+
+    }
