0c738fca6edc761d733acdb4f98eb92c3f29d5de
acepMunawar <acepmunawar96@gmail.com>
2020-03-09 17:18:59 -0400

    finis activity home

---
+++ app/src/main/java/com/Education/dasarkode/belajar/home_belajar.java
@@ -0,0 +1,16 @@
+package com.Education.dasarkode.belajar;
+
+import com.Education.dasarkode.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class home_belajar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_belajar);
+    }
+}
