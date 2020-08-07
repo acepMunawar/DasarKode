bfa49ee9c845df580eb51848100c5c019233aeb6
acepMunawar <acepmunawar96@gmail.com>
2020-08-07 20:45:18 +0700

    finish fragment Register, profile, edite profile and forgot paswword

---
+++ app/src/main/java/com/Education/dasarkode/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.Education.dasarkode.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/Education/dasarkode/ui/main/ProfileFragment.java
@@ -0,0 +1,39 @@
+package com.Education.dasarkode.ui.main;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.Education.dasarkode.R;
+
+public class ProfileFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+
+    public static ProfileFragment newInstance() {
+        return new ProfileFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
