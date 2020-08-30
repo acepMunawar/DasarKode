e691b7abd9be1f2094599cc6e982d96b7381ec73
acepmunawar <acepmunawar96@gmail.com>
2020-08-30 02:18:11 +0100

    on prosess page materi java

--- app/src/main/java/com/education/dasarkode/UserInterface/Info/Fragment_dashboard_info.java
+++ app/src/main/java/com/education/dasarkode/UserInterface/Info/Fragment_dashboard_info.java
@@ -1,31 +1,116 @@
 package com.education.dasarkode.UserInterface.Info;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.education.dasarkode.R;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class Fragment_dashboard_info extends Fragment {
-
-    public Fragment_dashboard_info() {
-        // Required empty public constructor
-    }
-
-
+    Button firstButtonPlaying;
+    SeekBar firstSeekBar;
+    TextView firstElapsedTimeLabel;
+    TextView firstRemainingTimeLabel;
+    MediaPlayer firstMediaPlayer;
+    int firstTotalTime;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_dashboard_info, container, false);
+
+        //first sound button
+        firstButtonPlaying= (Button) view.findViewById(R.id.btnInfo2);
+        firstElapsedTimeLabel = (TextView) view.findViewById(R.id.ETLInfo2);
+        firstRemainingTimeLabel = (TextView) view.findViewById(R.id.RTLInfo2);
+        firstMediaPlayer = MediaPlayer.create(getActivity(), R.raw.info);
+        firstMediaPlayer.seekTo(0);
+        firstTotalTime = firstMediaPlayer.getDuration();
+        firstSeekBar =(SeekBar) view.findViewById(R.id.seekbarInfo2);
+        btnSound1();
         return view;
     }
+
+
+    //    method btn sound 1
+    public void btnSound1(){
+        firstSeekBar.setMax(firstTotalTime);
+        firstSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser){
+                    firstMediaPlayer.seekTo(progress);
+                    firstSeekBar.setProgress(progress);
+                }
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (firstMediaPlayer != null){
+                    try {
+                        Message msg =new Message();
+                        msg.what = firstMediaPlayer.getCurrentPosition();
+                        handler.sendMessage(msg);
+                        Thread.sleep(1000);
+                    }catch (InterruptedException e){
+                    }
+                }
+            }
+        }).start();
+        firstButtonPlaying.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!firstMediaPlayer.isPlaying()){
+                    firstMediaPlayer.start();
+                    firstButtonPlaying.setBackgroundResource(R.drawable.pause_sound);
+                }else{
+                    firstMediaPlayer.pause();
+                    firstButtonPlaying.setBackgroundResource(R.drawable.play_sound);
+                }
+            }
+        });
+    }
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            int currentPosition = msg.what;
+            firstSeekBar.setProgress(currentPosition);
+            String elapsedTime = firstCreateTimeLabel(currentPosition);
+            firstElapsedTimeLabel.setText(elapsedTime);
+            String remainingTime = firstCreateTimeLabel(firstTotalTime-currentPosition);
+            firstRemainingTimeLabel.setText("-" + remainingTime);
+        }
+    };
+
+    public String firstCreateTimeLabel(int time){
+        String timeLabel = "";
+        int min = time / 1000 /60;
+        int sec =time /1000 % 60;
+        timeLabel = min + "";
+        if(sec <10) timeLabel +="0";
+        timeLabel += sec;
+        return timeLabel;
+    }
+
 }
